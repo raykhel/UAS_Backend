eadbbd09f592634482ce46161b27f47cbced2e8c
raykhel <raykhelnatanael05@gmail.com>
2025-06-16 20:07:24 +0700

    buat database dan web

--- ecommerce/database/seeders/DatabaseSeeder.php
+++ ecommerce/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        $this->call(ProductSeeder::class);
 
         User::factory()->create([
             'name' => 'Test User',
